b63bddbf1b2289516d25dc39d2ee6ff1c5dcc021
Denis Papathanasiou <denis@papathanasiou.org>
2012-08-29 12:32:42 -0400

    Added an Init() func to allow different servers to change the reCaptcha private key for each domain w/o editing the source and rebuilding. Updated the func documentation to conform to the Go guidelines for documenting code (http://golang.org/doc/articles/godoc_documenting_go_code.html) Passed the source through gofmt.

--- recaptcha.go
+++ recaptcha.go
@@ -7,11 +7,11 @@
 package recaptcha
 
 import (
-    "net/http"
-    "net/url"
-    "io/ioutil"
-    "strings"
-    "log"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 const recaptcha_server_name = "http://www.google.com/recaptcha/api/verify"
@@ -22,35 +22,35 @@ var recaptcha_private_key string
 // and the client's response input to that challenge to determine whether or not
 // the client answered the reCaptcha input question correctly.
 // It returns a boolean value indicating whether or not the client answered correctly.
-func check (remoteip, challenge, response string) (s string) {
-    s = ""
-    resp, err := http.PostForm(recaptcha_server_name,
-    	url.Values{"privatekey": {recaptcha_private_key}, "remoteip": {remoteip}, "challenge": {challenge}, "response": {response}})
-    if err != nil {
-    	log.Println("Post error: %s", err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-    	log.Println("Read error: could not read body: %s", err)
-    } else {
-    	s = string(body)
-    }
-    return
-}	
+func check(remoteip, challenge, response string) (s string) {
+	s = ""
+	resp, err := http.PostForm(recaptcha_server_name,
+		url.Values{"privatekey": {recaptcha_private_key}, "remoteip": {remoteip}, "challenge": {challenge}, "response": {response}})
+	if err != nil {
+		log.Println("Post error: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Read error: could not read body: %s", err)
+	} else {
+		s = string(body)
+	}
+	return
+}
 
 // Confirm is the public interface function.
 // It calls check, which the client ip address, the challenge code from the reCaptcha form,
 // and the client's response input to that challenge to determine whether or not
 // the client answered the reCaptcha input question correctly.
 // It returns a boolean value indicating whether or not the client answered correctly.
-func Confirm (remoteip, challenge, response string) (result bool) {
-    result = strings.HasPrefix(check(remoteip, challenge, response), "true") 
-    return
+func Confirm(remoteip, challenge, response string) (result bool) {
+	result = strings.HasPrefix(check(remoteip, challenge, response), "true")
+	return
 }
 
 // Init allows the webserver or code evaluating the reCaptcha form input to set the
 // reCaptcha private key (string) value, which will be different for every domain.
-func Init (key string) {
+func Init(key string) {
 	recaptcha_private_key = key
 }
