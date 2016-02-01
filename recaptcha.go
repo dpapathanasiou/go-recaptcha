200d5f142b57900053c623c93c7b9f5a1eaffa6e
Grim <GrimTheReaper@users.noreply.github.com>
2016-02-01 09:53:52 -0600

    Properly Formatted
    Please, don't use `fmt.Println` when you have a variable you are also printing out, use `fmt.Printf`.

--- recaptcha.go
+++ recaptcha.go
@@ -3,7 +3,7 @@
 // This package is designed to be called from within an HTTP server or web framework
 // which offers reCaptcha form inputs and requires them to be evaluated for correctness
 //
-// Edit the recaptcha_private_key constant before building and using
+// Edit the recaptchaPrivateKey constant before building and using
 package recaptcha
 
 import (
@@ -22,19 +22,19 @@ type RecaptchaResponse struct {
 	ErrorCodes []int `json:"error-codes"`
 }
 
-const recaptcha_server_name = "https://www.google.com/recaptcha/api/siteverify"
+const recaptchaServerName = "https://www.google.com/recaptcha/api/siteverify"
 
-var recaptcha_private_key string
+var recaptchaPrivateKey string
 
 // check uses the client ip address, the challenge code from the reCaptcha form,
 // and the client's response input to that challenge to determine whether or not
 // the client answered the reCaptcha input question correctly.
 // It returns a boolean value indicating whether or not the client answered correctly.
 func check(remoteip, response string) (r RecaptchaResponse) {
-	resp, err := http.PostForm(recaptcha_server_name,
-		url.Values{"secret": {recaptcha_private_key}, "remoteip": {remoteip}, "response": {response}})
+	resp, err := http.PostForm(recaptchaServerName,
+		url.Values{"secret": {recaptchaPrivateKey}, "remoteip": {remoteip}, "response": {response}})
 	if err != nil {
-		log.Println("Post error: %s", err)
+		log.Printf("Post error: %s\n", err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -61,5 +61,5 @@ func Confirm(remoteip, response string) (result bool) {
 // Init allows the webserver or code evaluating the reCaptcha form input to set the
 // reCaptcha private key (string) value, which will be different for every domain.
 func Init(key string) {
-	recaptcha_private_key = key
+	recaptchaPrivateKey = key
 }
